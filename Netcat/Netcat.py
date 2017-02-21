2584a874cc8da7ff0b235250c61cb17bc7f1067a
vrioche <soumille.lucas@etu.unice.fr>
2017-02-21 18:58:57 +0100

    end netcat

--- Netcat/Netcat.py
+++ Netcat/Netcat.py
@@ -99,5 +99,68 @@ def client_sender(buffer):
         client.close()
 
 def server_loop():
-    print "ToDO"
+    global target
+
+    if not len(target):
+        target = "0.0.0.0" #listen on all interfaces with an IPv4 address
+
+    server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    server.bind((target, port))
+    server.listen(3)
+
+    while True:
+        client_socket, addr = server.accept()
+        #Launch the client in a new thread
+        client_thread = threading.Thread(target=client_handler, args=(client_socket,))
+        client_thread.start()
+
+def run_command(cmd):
+    #Trim
+    cmd = cmd.rstrip()
+    try:
+        output = subprocess.check_output(cmd, stderr=subprocess.STDOUT, shell=True)
+    except:
+        output = "Failed to execute command.\r\n"
+
+    return output
+
+def client_handler(client_socket):
+    global upload
+    global execute
+    global command
+
+    #Need to perform an upload
+    if len(upload_destination):
+        file_buffer = ""
+        #Fulfill the buffer with the cleint data
+        while True:
+            data = client_socket.recv(1024)
+            if not data:
+                break
+            else:
+                file_buffer += data
+        #Write the buffer in the file
+        try:
+            file_descriptor = open(upload_destination, "wb")
+            file_descriptor.write(file_buffer)
+            file_descriptor.close()
+            client_socket.send("Successfully saved file to %s\r\n" % upload_destination)
+        except:
+            client_socket.send("Failed to save file to %s\r\n" % upload_destination)
+
+    #Need to execute a command
+    if len(execute):
+        output = run_command(execute)
+        client_socket.send(output)
+
+    #Need to emulate a shell
+    if command:
+        while True:
+            client_socket.send("$>")
+            cmd_buffer = ""
+            #get the cmd
+            while "\n" not in cmd_buffer:
+                cmd_buffer += client_socket.recv(1024)
+            output = run_command(cmd_buffer)
+            client_socket.send(output)
 main()
